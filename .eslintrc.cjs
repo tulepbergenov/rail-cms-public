da8bba060080f38323f9ac09f715362f3c955dc9
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-06-29 18:12:05 +0500

    chore: add linters, alias

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,33 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
+    "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: [
+    "react-refresh",
+    "perfectionist",
+    "styled-components-a11y",
+    "prettier",
+  ],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "perfectionist/sort-jsx-props": [
+      "error",
+      {
+        type: "natural",
+        order: "asc",
+        groups: ["multiline", "unknown", "shorthand"],
+      },
     ],
   },
 };
